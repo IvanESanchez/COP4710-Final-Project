88a30f9ff14c81f0491ad5f58c9dcab433a5f120
Rob Schwyzer <TerranUp16@hotmail.com>
2021-11-19 19:43:34 -0500

    Implemented db.php
    Shuffled some code from db-example.php to db.php and reworked db-example.php to require('./db.php') to fill the gap. Now db.php can be included via require into any PHP script which needs to open a connection to MariaDB

--- Stack/app/db-example.php
+++ Stack/app/db-example.php
@@ -16,19 +16,8 @@
         <th>Database Name</th>
       </tr>
       <?php
-        // Fetch environment-specific connection data
-        $mariadb_username = getenv('MARIADB_USER');
-        $mariadb_password = file_get_contents('/run/secrets/mariadb-password');
-        $mariadb_database = getenv('MARIADB_DATABASE');
-
-        // You should enable error reporting for mysqli before attempting to make a connection
-        mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
-
-        // Initiate connection
-        $mysqli = new mysqli('db', $mariadb_username, $mariadb_password, $mariadb_database, 3306);
-
-        // Set the desired charset after establishing a connection
-        $mysqli->set_charset('utf8mb4');
+        // Obtain database connection
+        require('./db.php');
         
         // Perform query and store in $result
         $result = $mysqli->query("SHOW DATABASES;");
