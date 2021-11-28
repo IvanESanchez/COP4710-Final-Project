d5967d4bf91dbf6c2363e9409ad8dce0c2ab3961
Rob Schwyzer <TerranUp16@hotmail.com>
2021-11-28 17:47:21 -0500

    Implement mailjet

---
+++ Stack/app/mail.php
@@ -0,0 +1,34 @@
+<?php 
+  require 'mailjet/vendor/autoload.php';
+
+  use \Mailjet\Resources;
+
+  function send_email($target, $subject, $text_body, $html_body) {
+    $public_key = file_get_contents('/run/secrets/email-public-key');
+    $private_key = file_get_contents('/run/secrets/email-private-key');
+    $mj = new \Mailjet\Client($public_key,$private_key,true,['version' => 'v3.1']);
+
+    $body = [
+      'Messages' => [
+        [
+          'From' => [
+            'Email' => "rschwyzer@knights.ucf.edu",
+            'Name' => "Rob"
+          ],
+          'To' => [
+            [
+              'Email' => $target,
+              'Name' => "User"
+            ]
+          ],
+          'Subject' => $subject,
+          'TextPart' => $text_body,
+          'HTMLPart' => $html_body
+        ]
+      ]
+    ];
+    
+    $response = $mj->post(Resources::$Email, ['body' => $body]);
+    return $response->success();
+  }
+?>
---
+++ Stack/app/mailjet-test.php
@@ -0,0 +1,10 @@
+<?php
+  require 'mail.php';
+
+  $receiver = 'weposi1177@nefacility.com';
+  $email_subject = 'COP 4710 Team 13 Final Project';
+  $email_text_body = "If sendmail works, then Rob will receive this. If not, well...\n\n-Team 13";
+  $email_html_body = "If sendmail works, then Rob will receive this. If not, well...<br><br>-Team 13";
+
+  send_email($receiver, $email_subject, $email_text_body, $email_html_body);
+?>
