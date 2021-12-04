0cc9777defd19ce1eaa449e95ede6800a53713fd
Andy <andygr4200@gmail.com>
2021-12-04 16:36:29 -0500

    Fix main menu hickup

--- Stack/app/templates/navbar.php
+++ Stack/app/templates/navbar.php
@@ -6,7 +6,7 @@
     <div class="side-bar">
       <h2>Menu<ul>Options</h2>
       <ul>
-        <li><a href="MainMenu.html"><img src="house-fill.svg" alt="Home" width="15"> Home</a></li>
+        <li><a href="MainMenu.php"><img src="house-fill.svg" alt="Home" width="15"> Home</a></li>
         <li><a href="BookForm.html"><img src="book.svg" alt="book-order" width="15"> Book request form</a></li>
         <li><a href="#"><img src="list.svg" alt="book-request" width="15"> View book forms</a></li>
         <li><a href="#"><img src="bookshelf.svg" alt="book-list" width="15"> Edit book forms</a></li>
