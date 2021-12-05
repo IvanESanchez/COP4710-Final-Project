a2ea90555f0d143be738e735461b2424e0dda8fc
Rob Schwyzer <TerranUp16@hotmail.com>
2021-12-05 16:17:01 -0500

    Forgot password now works

--- Stack/app/ForgotPassword.php
+++ Stack/app/ForgotPassword.php
@@ -4,6 +4,7 @@
 		// Load requirements
 		require $_SERVER["DOCUMENT_ROOT"] . '/functions/mail.php';
 		require $_SERVER["DOCUMENT_ROOT"] . '/functions/db.php';
+		require $_SERVER["DOCUMENT_ROOT"] . '/functions/show_feedback.php';
 
 		// Generate temporary password to use
 		$temporary_password = $mysqli->real_escape_string('123abcCH@NGEMENOW');
@@ -12,19 +13,42 @@
 		$receiver = $mysqli->real_escape_string(trim($_POST['email']));
 
 		// Check if account exists
-		$query = "SELECT uid FROM USER WHERE email = " . $receiver . ";";
+		$query = "SELECT uid FROM USER WHERE email = '" . $receiver . "';";
 
 		try {
+			// Run query
 			$result = $mysqli->query($query);
 
+			// Check if any results found
+			if ($result->num_rows > 0) {
+				// Get uid of first result
+				$row = $result->fetch_assoc();
+				$uid = $row["uid"];
 
+				// Update password for uid
+				$query = "UPDATE USER SET password = '" . $temporary_password . "' WHERE uid = " . $uid . ";";
+				$mysqli->query($query);
+
+				// Email new password
+				$email_subject = "Password reset for Book Store at UCF";
+				$email_text_body = "Your new password is: " . $temporary_password;
+				$email_html_body = "Your new password is: <code>" . $temporary_password . "</code>";
+				if (send_email($receiver, $email_subject, $email_text_body, $email_html_body)) {
+					// If email sends successfully, redirect to login page
+					header('Location: login.php');
+				} else {
+					// If email doesn't send successfully show error
+					show_error("Failed to send email to <code>" . $receiver . "</code>");
+				}
+			} else {
+				// Redirect silently; do not want to let an attacker know the account doesn't exist
+				header('Location: login.php');
+			}
+		} catch (mysqli_sql_exception $e) {
+			show_error($mysqli->error);
+		} finally {
+			$mysqli->close();
 		}
-		
-		$email_subject = 'Password reset for Book Store at UCF';
-		$email_text_body = "If sendmail works, then Rob will receive this. If not, well...\n\n-Team 13";
-		$email_html_body = "If sendmail works, then Rob will receive this. If not, well...<br><br>-Team 13";
-	
-		send_email($receiver, $email_subject, $email_text_body, $email_html_body);
 	}
 ?>
 
@@ -47,7 +71,7 @@
 
 	<main class = "form-newuser">
 
-		<form action = "index.html" method = "get">
+		<form action = "ForgotPassword.php" method = "post">
 
 			<h1 class = "mb-3">Password Reset</h1>
 			<h2 class = "h4 mb-4 fw-normal">Enter Account Email</h1>
@@ -57,14 +81,13 @@
 				placeholder="name@example.com">
 			  <label for="floatingInput">Email Address</label>
 			</div>
-
-			<button class = "mt-3 mb-1 w-50 btn-lg btn-primary"
-			type = "submit">Reset Password</button>
+			
+			<input type = "submit" class = "mt-3 mb-1 w-50 btn-lg btn-primary" value="Reset Password">
 
 			<div class = "mt-1 redirect">
 				<p>Need to Create a new account?
 					 <a href= "NewUser.php">click here</a>
-					 <br/>Sign in<a href="index.php"> here</a>
+					 <br/>Sign in<a href="login.php"> here</a>
 				</p>
 			</div>
 
