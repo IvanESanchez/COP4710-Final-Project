665d315ac8cb31b188be841a9c76e6bd332932f6
Andy <andygr4200@gmail.com>
2021-12-04 21:14:19 -0500

    stage

--- Stack/app/login.php
+++ Stack/app/login.php
@@ -1,3 +1,20 @@
+<?php
+  if (!empty($_POST['username']) and !empty($_POST['password'])) {
+    $username = trim($_POST['username']);
+    $password = trim($_POST['password']);
+
+    // Need to check database
+
+
+    // Assuming that comes back okay...
+    require "functions/no_cookies.php";
+    $_SESSION['username'] = $username;
+    $_SESSION['password'] = $password;
+    session_write_close();
+    header('Location: index.php');
+  }
+?>
+
 <!DOCTYPE html>
 <html lang = "en">
 <head>
@@ -19,7 +36,7 @@
 
 	<main class = "form-signin">
 		<!-- This is the sign in form that contain user information -->
-		<form action="<?php echo $_SERVER['PHP_SELF'];?>" method="POST">
+		<form action = "MainMenu.html" method="post">
 
 			<h1 class = "mb-2">Book Store At</h1>
 
@@ -30,21 +47,21 @@
 			<h1 class = "h3 mb-3 fw-normal">User Login</h1>
 
 			<div class="form-floating mb-3">
-		  	<input type="text" class="form-control" name="loginName" id="floatingInput"
-				placeholder="Name">
+		  	<input type="text" class="form-control" id="floatingInput"
+				placeholder="Username">
 		  	<label for="floatingInput">Name</label>
 			</div>
 
 			<div class="form-floating mb-3">
-		  	<input type="password" class="form-control" name="loginPass" id="floatingInput"
-				placeholder="Password">
-		  	<label for="floatingInput">Password</label>
+		  	<input type="password" class="form-control" id="floatingPassword"
+				 placeholder="Password">
+		  	<label for="floatingPassword">Password</label>
 			</div>
 
 			<div>
-				<button class = "mt-2 mb-2 w-50 btn-lg btn-primary"
-				type = "submit">Sign in
-				</button>
+					<button class = "mt-2 mb-2 w-50 btn-lg btn-primary"
+					type = "submit">Sign in
+				  </button>
 			</div>
 
 			<div class = "new-user">
@@ -56,43 +73,6 @@
 			</div>
 
 		</form>
-
-		<?php
-
-			if (!empty($_POST['loginName']) and !empty($_POST['loginPass'])) {
-
-		    	$name = trim($_POST['loginName']);
-		    	$password = trim($_POST['loginPass']);
-
-			    // Need to check database
-				require 'functions/db.php';
-
-				// Perform Query and store in $result
-				$result = $mysqli->query('SELECT * FROM user U;');
-
-				// Loop through results
-				$valid_user = False;
-
-				while ($row = $result->fetch_row()) {
-					if ($row[1] == $name and $row[3] == $password) {
-						$valid_user = True;
-						$admin = $row[4];
-						break;
-					}
-				}
-
-				if ($valid_user == True) {
-			    	// Assuming that comes back okay...
-			    	require "functions/no_cookies.php";
-			    	$_SESSION['name'] = $name;
-			    	$_SESSION['admin'] = $admin;
-			    	session_write_close();
-			    	header('Location: index.php');
-					exit;
-				}
-			}
-
-		?>
 
 	</main>
 
