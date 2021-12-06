264a5f4c8ee5c29cb5170e4bb0c2b8fa5f266be7
Andy <andygr4200@gmail.com>
2021-12-06 13:42:14 -0500

    Manage reminders

--- Stack/app/ManageReminders.php
+++ Stack/app/ManageReminders.php
@@ -13,8 +13,26 @@
   <link rel = "stylesheet" href = "css/base.css">
   <link rel = "stylesheet" href = "css/navbar.css">
   <link rel = "stylesheet" href = "css/wrapper.css">
-  <!-- <link rel = "stylesheet" href = "css/admin.css"> -->
-	<link rel = "stylesheet" href = "css/admin-menu.css">
+
+	<style>
+		.wrapper .Center-section .main-text .btn-primary{
+			margin-left: 3%;
+			width: 500px;
+		}
+
+		.wrapper .Center-section .main-text{
+			padding-bottom: 100px;
+		}
+
+		.wrapper .Center-section .table{
+			margin-left: 22%;
+			width:50%;
+		}
+
+
+	</style>
+
+
 </head>
 <body>
 
@@ -27,12 +45,31 @@
       <div class="header h3 mt-3 mb-4">Manage Reminders</div>
 
       <div class="main-text">
-
-
+				<form action="CreateReminder.php" method="get">
+					<div class="mt-2 btn">
+							<button class = "mb-1 btn-lg btn-primary"
+							type = "submit"> Create New Reminder
+						  </button>
+					</div>
+				</form>
       </div>
 
+			<table class="mt-4 table">
+				<thead class="table-dark">
+					<tr align="center">
+						<th>Id</th>
+						<th>Date</th>
+						<th>Delete</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+					</tr>
+				</tbody>
+			</table>
 
 		</div>
+
    </div>
 
 </body>
