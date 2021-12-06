475d0c9c436947e8d0c576a04007963e72db656f
Rob Schwyzer <TerranUp16@hotmail.com>
2021-12-05 22:43:47 -0500

    Enable user deletion

--- Stack/app/DeleteUser.php
+++ Stack/app/DeleteUser.php
@@ -9,13 +9,19 @@
       // Save uid
       $uid = $_GET["uid"];
 
-      // Delete given uid
+      // Start target url
+      $url = "ManageUsers.php?operation=delete&uid=" . $uid;
+
+      // Delete given uid and track whether operation succeeded or not
       require_once $_SERVER["DOCUMENT_ROOT"] . '/functions/account_management.php';
       if (delete_uid($uid)) {
-
+        $url = $url . "&result=success";
       } else {
-        
+        $url = $url . "&result=fail" ;
       }
+
+      // Redirect user back to management with appropriate data for management UX to give feedback
+      header('Location: ' . $url);
     } else {
       // Redirect to user management
       header('Location: ManageUsers.php');
