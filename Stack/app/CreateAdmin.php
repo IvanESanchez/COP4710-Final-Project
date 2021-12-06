a0ada4d7b17d4f5a7b3130fb5b5b4765322dc369
Andy <andygr4200@gmail.com>
2021-12-06 02:05:07 -0500

    Changes

--- Stack/app/CreateAdmin.php
+++ Stack/app/CreateAdmin.php
@@ -11,55 +11,49 @@
 	<title>Create Account</title>
 
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-	<link rel = "stylesheet" href = "css/create-admin.css">
 	<link rel = "stylesheet" href = "css/base.css">
   <link rel = "stylesheet" href = "css/navbar.css">
 	<link rel = "stylesheet" href = "css/wrapper.css">
 </head>
-<body class = "text-center">
+<body>
 
 	<div class="wrapper">
 
 	<?php
 		include $_SERVER["DOCUMENT_ROOT"] . '/templates/adminbar.php';
 	?>
+		<div class="Center-section">
+			<form action = "#" method ="get">
 
+				<h1 class = "mb-4">Create an Admin account</h1>
 
-	<main class = "form-newuser">
+				<div class = "mb-2 form-floating">
+					<input type="text" class="form-control" id="floatingInput"
+					placeholder="Name">
+			  	<label for="floatingInput">Name</label>
+				</div>
 
-		<form action = "#" method ="get">
+				<div class="mb-2 form-floating">
+				  <input type="email" class="form-control" id="floatingInput"
+					placeholder="name@example.com">
+				  <label for="floatingInput">Email Address</label>
+				</div>
 
-			<h1 class = "mb-4">Create an Admin account</h1>
+				<div class = "mb-2 form-floating">
+					<input type = "password" class="form-control" id="floatingInput"
+					placeholder="Password">
+					<label for="floatingInput">Password</label>
+				</div>
 
-			<div class = "mb-2 form-floating">
-				<input type="text" class="form-control" id="floatingInput"
-				placeholder="Name">
-		  	<label for="floatingInput">Name</label>
-			</div>
+				<div class = "mb-2 form-floating">
+					<input type = "checkbox"> Check to confirm admin status<br/>
+				</div>
 
-			<div class="mb-2 form-floating">
-			  <input type="email" class="form-control" id="floatingInput"
-				placeholder="name@example.com">
-			  <label for="floatingInput">Email Address</label>
-			</div>
-
-			<div class = "mb-2 form-floating">
-				<input type = "password" class="form-control" id="floatingInput"
-				placeholder="Password">
-				<label for="floatingInput">Password</label>
-			</div>
-
-			<div class = "mb-2 form-floating">
-				<input type = "checkbox"> Check to confirm admin status<br/>
-			</div>
-
-			<button class = "mt-3 mb-1 w-50 btn-lg btn-primary"
-			type = "submit">Create account</button>
-
+				<button class = "mt-3 mb-1 w-50 btn-lg btn-primary"
+				type = "submit">Create account</button>
 
 		</form>
-
-	<main>
+	 </div>
 	</div>
 </body>
 
