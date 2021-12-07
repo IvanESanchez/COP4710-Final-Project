43bd90f52dd20ed4993f9f4f21136cd6e5a3f7b2
Rob Schwyzer <TerranUp16@hotmail.com>
2021-12-07 01:28:01 -0500

    Update FinalReport.php

--- Stack/app/FinalReport.php
+++ Stack/app/FinalReport.php
@@ -13,60 +13,65 @@
     <link rel = "stylesheet" href = "css/base.css">
   </head>
   <body>
-    <table class="table table-dark table-hover">
-      <tbody>
-        <?php
-          require_once $_SERVER["DOCUMENT_ROOT"] . '/functions/get_utils.php';
-          require_once $_SERVER["DOCUMENT_ROOT"] . '/functions/book_request_management.php';
+    <div class="container">
+      <table class="table table-dark table-striped">
+        <tbody class="text-center">
+          <?php
+            require_once $_SERVER["DOCUMENT_ROOT"] . '/functions/get_utils.php';
+            require_once $_SERVER["DOCUMENT_ROOT"] . '/functions/book_request_management.php';
 
-          // Get professors
-          $professors = get_all_professors_with_requests();
+            // Get professors
+            $professors = get_all_professors_with_requests();
 
-          // Loop through professors
-          foreach($professors as $professor) {
-            // Get professor's name
-            $name = get_name($professor);
+            // Loop through professors
+            foreach($professors as $professor) {
+              // Get professor's name
+              $name = get_name($professor);
 
-            // Set name to some default if one wasn't retrieved
-            if ($name == null) {
-              $name = 'MissingNo';
-            }
+              // Set name to some default if one wasn't retrieved
+              if ($name == null) {
+                $name = 'MissingNo';
+              }
 
-            // Make a header row for the professor
-            echo '<tr><th colspan="5">' . $name . '</th></tr>';
+              // Make a header row for the professor
+              echo '<tr><th colspan="5">Professor ' . $name . '</th></tr>';
 
-            // Get semesters this professor has book lists for
-            $semester_ids = get_reqs_semesters_for_uid($uid);
+              // Get semesters this professor has book lists for
+              $semester_ids = get_reqs_semesters_for_uid($professor);
 
-            // Get semester components (year and season) sorted, ascending
-            $semesters = get_semesters_sorted_asc($semester_ids);
+              // Get semester components (year and season) sorted, ascending
+              $semesters = get_semesters_sorted_asc($semester_ids);
 
-            // Loop through semesters
-            foreach($semesters as $semester) {
-              // Output semester to table
-              echo '<tr><td colspan="5">' . $semester['season'] . " " .  $semester['year'] . '</td></tr>';
+              // Loop through semesters
+              foreach($semesters as $semester) {
+                // Output semester to table
+                echo '<tr><td colspan="5">Semester ' . $semester['season'] . " " .  $semester['year'] . '</td></tr>';
 
-              // Get the book request ID for this semeser for this professor
-              $brid = pull_brid($semester['skey'], $professor);
+                // Get the book request ID for this semeser for this professor
+                $brid = pull_brid($semester['skey'], $professor);
 
-              // Get book IDs for this brid
-              $book_list = get_book_list($brid);
+                // Get book IDs for this brid
+                $book_list = get_book_list($brid);
 
-              // Output each book to the table
-              foreach($book_list as $book) {
-                // Get book's data
-                $book_data = get_book_data($book);
+                // Output header row to clarify what below fields are
+                echo '<tr><th>Title</th><th>Author(s)</th><th>Edition</th><th>Publisher</th><th>ISBN</th></tr>';
 
-                // Create table row
-                echo '<tr><td>' . $book_data['title'] . '</td><td>' .
-                $book_data['author'] . '</td><td>' . $book_data['edition'] .
-                '</td><td>' . $book_data['publisher'] . '</td><td>' .
-                $book_data['isbn'] . '</td></tr>'
+                // Output each book to the table
+                foreach($book_list as $book) {
+                  // Get book's data
+                  $book_data = get_book_data($book);
+
+                  // Create table row
+                  echo '<tr><td>' . $book_data['title'] . '</td><td>' .
+                  $book_data['author'] . '</td><td>' . $book_data['edition'] .
+                  '</td><td>' . $book_data['publisher'] . '</td><td>' .
+                  $book_data['isbn'] . '</td></tr>';
+                }
               }
             }
-          }
-        ?>
-      </tbody>
-    </table>    
+          ?>
+        </tbody>
+      </table>
+    </div>
   </body>
 </html>
