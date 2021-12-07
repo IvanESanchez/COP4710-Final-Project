098da8efaa04927ef55d51e40170ce0da60031c5
Gavin Gilbert <ashensilverwolf@gmail.com>
2021-12-07 01:35:05 -0500

    adminbar book request form and view book forms rerouted

--- Stack/app/templates/adminbar.php
+++ Stack/app/templates/adminbar.php
@@ -7,8 +7,8 @@
     <h2>Menu<ul>Options</h2>
     <ul>
       <li><a href="AdminMenu.php"><img src="house-fill.svg" alt="Home" width="15"> Admin Menu</a></li>
-      <li><a href="AdminForm.php"><img src="book.svg" alt="book-order" width="15"> Book Request Form</a></li>
-      <li><a href="AdminView.php"><img src="list.svg" alt="book-request" width="15"> View Book Forms</a></li>
+      <li><a href="BookForm.php"><img src="book.svg" alt="book-order" width="15"> Book Request Form</a></li>
+      <li><a href="semestersearch.php"><img src="list.svg" alt="book-request" width="15"> View Book Forms</a></li>
       <li><a href="ManageAccount.php"><img src ="person-circle.svg" alt="account" width="15"> Manage Account</a></li>
       <li><a href="logout.php">Log Out</a></li>
     </ul>
