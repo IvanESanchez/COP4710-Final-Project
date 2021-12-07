5d07f272df74862975fb0f65afddef7b13d1400a
Andy <andygr4200@gmail.com>
2021-12-06 21:22:33 -0500

    update to style form data

--- Stack/app/FormData.php
+++ Stack/app/FormData.php
@@ -12,13 +12,33 @@
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
   <link rel = "stylesheet" href = "css/base.css">
 	<link rel = "stylesheet" href = "css/navbar.css">
-	<link rel = "stylesheet" href = "css/wrapper.css">
-	<link rel = "stylesheet" href = "css/book-form.css">
+	<!-- <link rel = "stylesheet" href = "css/wrapper.css"> -->
+	<!-- <link rel = "stylesheet" href = "css/book-form.css"> -->
 
 	<style>
+		.wrapper .Center-section .main-text {
+			margin-left: 38%;
+			padding-top: 20px;
+			font-size: 20px;
+		}
+
+		.wrapper .Center-section .main-text {
+			padding-top: 45px;
+			max-width: 550px;
+		}
+
 		.wrapper .Center-section .table{
 			margin-left: 15%;
 			width: 75%;
+		}
+
+		.btn{
+
+		}
+
+		.btn {
+		  width: 350px;
+			margin-left: 15%;
 		}
 
 
@@ -28,12 +48,27 @@
 		}
 
 		.wrapper .Center-section .main-text .form-dropdown{
+			margin-left: 5%;
 			width: 500px;
 		}
 
-		form{
-
+		select{
+			width: 450px;
+			height: 50px;
+			text-align: center;
+			font-size: 35px;
 		}
+		.words{
+			margin-left: 28%;
+			font-size: 35px;
+		}
+
+		.header{
+			margin-left: 45%;
+		}
+
+
+
 	</style>
 
 </head>
@@ -66,7 +101,8 @@
 				</tbody>
 			</table>
 
-      <div class="main-text"> Insert Data:
+      <div class="main-text">
+				<div class="words">Insert Data:</div>
 
 				<form action="#" method="get">
 
@@ -79,7 +115,7 @@
 						</select>
 					</div>
 
-					<div class="mt-3 btn">
+					<div class="btn">
 							<button class = "mt-2 mb-1 w-100 btn-lg btn-primary"
 							type = "submit">Submit
 						  </button>
@@ -93,7 +129,7 @@
 						<label for="floatingInput">Semester year</label>
 					</div>
 
-					<div class="mt-3 btn">
+					<div class="btn">
 							<button class = "mt-2 mb-1 w-100 btn-lg btn-primary"
 							type = "submit">Submit
 						  </button>
@@ -101,14 +137,14 @@
 				</form>
 
 				<form action="#" method="get">
-					<div class = "mb-1 form-floating">
+					<div class = "form-floating">
 						<input type="text" class="form-control" id="floatingInput"
 						placeholder="Book Title">
 						<label for="floatingInput">Book Title</label>
 					</div>
 
 					<div class="mt-3 btn">
-							<button class = "mt-2 mb-1 w-100 btn-lg btn-primary"
+							<button class = "mb-1 w-100 btn-lg btn-primary"
 							type = "submit">Submit
 						  </button>
 					</div>
@@ -149,7 +185,7 @@
 						<label for="floatingInput">Publisher</label>
 					</div>
 
-					<div class="mt-3 btn">
+					<div class="btn">
 							<button class = "mt-2 mb-1 w-100 btn-lg btn-primary"
 							type = "submit">Submit
 						  </button>
@@ -157,13 +193,13 @@
 				</form>
 
 				<form action="#" method="get">
-					<div class = "mb-1 form-floating">
+					<div class = "form-floating">
 						<input type="text" class="form-control" id="floatingInput"
 						placeholder="ISBN">
 						<label for="floatingInput">ISBN</label>
 					</div>
 
-					<div class="mt-3 btn">
+					<div class="btn">
 							<button class = "mt-2 mb-1 w-100 btn-lg btn-primary"
 							type = "submit">Submit
 						  </button>
